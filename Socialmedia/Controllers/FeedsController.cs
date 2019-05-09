f5e8b7ebe71d30cbdd147af7817d68ed8eb224c9
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-05-09 15:54:56 +0200

    FeedsController Get implemented, not yet tested

--- Socialmedia/Controllers/FeedsController.cs
+++ Socialmedia/Controllers/FeedsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Socialmedia.Models;
+using Socialmedia.Services;
 
 namespace Socialmedia.Controllers
 {
@@ -11,5 +13,26 @@ namespace Socialmedia.Controllers
     [ApiController]
     public class FeedsController : ControllerBase
     {
+        private readonly UserService _userService;
+        private readonly PostsService _postsService;
+
+        public FeedsController(UserService userService, PostsService postsService)
+        {
+            _userService = userService;
+            _postsService = postsService;
+        }
+
+        [HttpGet("{id")]
+        public ActionResult<List<Post>> Get(string userId)
+        {
+            List<Post> postsInFeed=new List<Post>();
+            User user = _userService.Get(userId);
+            foreach (var i in user.Post)
+            {
+                postsInFeed.Add(i);
+            }
+
+            return postsInFeed;
+        }
     }
 }
