b2e3a369490ecfbe2736c12ca6a6ac35b6ff3187
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-05-09 17:34:59 +0200

    CirclesController Get() + Create() created

---
+++ Socialmedia/Controllers/CirclesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Socialmedia.Models;
+using Socialmedia.Services;
+
+namespace Socialmedia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CirclesController : ControllerBase
+    {
+        private readonly CircleService _circleService;
+
+        [HttpGet]
+        public ActionResult<List<Circle>> Get()
+        {
+            return _circleService.Get();
+        }
+
+        [HttpPost]
+        public ActionResult<Circle> Create(Circle circle)
+        {
+            _circleService.Create(circle);
+            return CreatedAtRoute("GetCircle", new { id = circle.Id.ToString() }, circle);
+        }
+    }
+}
