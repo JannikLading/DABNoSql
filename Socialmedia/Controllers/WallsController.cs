86fcb85622b5aab038ea2b83253844c4732cc228
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-05-11 17:04:38 +0200

    WallsController HttpGet(user_id, guest_id) implemented, needs testing + validation.

--- Socialmedia/Controllers/WallsController.cs
+++ Socialmedia/Controllers/WallsController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Socialmedia.Models;
+using Socialmedia.Services;
+
 
 namespace Socialmedia.Controllers
 {
@@ -12,17 +14,50 @@ namespace Socialmedia.Controllers
     [ApiController]
     public class WallsController : ControllerBase
     {
-        private readonly UsersController _usersController;
+        private readonly UserService _userService;
+        private readonly CircleService _circleService;
 
-        public WallsController(UsersController usersController)
+        public WallsController(UserService userService, CircleService circleService)
         {
-            _usersController = usersController;
+            _userService = userService;
+            _circleService = circleService;
         }
 
         [HttpGet("user_id, guest_id")]
-        public ActionResult<Post> Get(string user_id, string guest_id)
+        public ActionResult<List<Post>> Get(string user_id, string guest_id)
         {
-            
+            var user = _userService.Get(user_id);
+
+            foreach (var i in user.FollowUserId)
+            {
+                if (i == guest_id)
+                    return NotFound();
+            }
+
+            List<Post> Posts = new List<Post>();
+            var guestUser = _userService.Get(guest_id);
+
+            foreach (var i in user.Post)
+            {
+                Posts.Add(i);
+            }
+
+            foreach (var i in user.CircleId)
+            {
+                foreach (var j in guestUser.CircleId)
+                {
+                    if (j==i)
+                    {
+                        var circle = _circleService.Get(i);
+                        foreach (var var in circle.Post)
+                        {
+                            if (var.UserId==user.Id)
+                                Posts.Add(var);
+                        }
+                    }
+                }
+            }
+            return Posts;
         }
     }
 }
