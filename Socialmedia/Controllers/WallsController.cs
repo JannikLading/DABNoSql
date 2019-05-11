513fe0dea824962f5e654d35a37473e01783877a
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-05-11 16:36:54 +0200

    WallsController created.

---
+++ Socialmedia/Controllers/WallsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Socialmedia.Models;
+
+namespace Socialmedia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WallsController : ControllerBase
+    {
+        private readonly UsersController _usersController;
+
+        public WallsController(UsersController usersController)
+        {
+            _usersController = usersController;
+        }
+
+        [HttpGet("user_id, guest_id")]
+        public ActionResult<Post> Get(string user_id, string guest_id)
+        {
+            
+        }
+    }
+}
